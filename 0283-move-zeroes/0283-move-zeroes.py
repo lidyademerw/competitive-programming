82524019b9b83214a2157f20c9ec6da7c9374dc0
lidya demerw <demerwlidya@gmail.com>
2026-02-16 16:39:32 -0800

    Time: 4 ms (61.2%), Space: 20.6 MB (46.39%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,10 +4,11 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
+        k=0
         for i in range(len(nums)):
-            if nums[i]==0:
-                nums.remove(0)
-                nums.append(0)
-              
+            if nums[i]!=0:
+                nums[i],nums[k]=nums[k],nums[i]
+                k+=1
+                
         
        
