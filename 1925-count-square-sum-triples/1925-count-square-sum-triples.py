1344f5532462963381d3a50c1d6757fc4a030018
lidya demerw <demerwlidya@gmail.com>
2026-04-05 00:12:14 -0700

    Time: 398 ms (18.62%), Space: 19.3 MB (72.92%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -3,7 +3,7 @@ class Solution:
         count=0
         for i in range(1,n+1):
             for j in range(1,n+1):
-                c=int(sqrt(pow(i,2) + pow(j,2)))
+                c=int(sqrt(pow(i,2) + pow(j,2) + 1))
                 if c <= n and pow(c,2)==pow(i,2) + pow(j,2):
                     count+=1
         return count
