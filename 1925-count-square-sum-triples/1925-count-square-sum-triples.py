72c2978e642117567a2e95456ec2da571fa36481
lidya demerw <demerwlidya@gmail.com>
2026-04-05 00:10:05 -0700

    Time: 375 ms (18.91%), Space: 19.1 MB (99.71%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        count=0
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                c=int(sqrt(pow(i,2) + pow(j,2)))
+                if c <= n and pow(c,2)==pow(i,2) + pow(j,2):
+                    count+=1
+        return count
+      
+        
