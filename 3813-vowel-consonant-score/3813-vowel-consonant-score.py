5838c74471b5be5dad57ce4d1783937b1f746b17
lidya demerw <demerwlidya@gmail.com>
2026-06-02 13:16:47 -0700

    Time: 2 ms (48.07%), Space: 19.3 MB (74.68%) - LeetHub

---
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.py
@@ -0,0 +1,17 @@
+class Solution:
+    def vowelConsonantScore(self, s: str) -> int:
+        m=['a', 'e', 'i', 'o', 'u']
+        v=0
+        c=0
+        for i in s:
+            if i in m and i.isalpha():
+                v+=1 
+            elif i.isalpha():
+                c+=1
+        if c > 0:
+            return v//c
+        else:
+            return 0
+
+
+        
