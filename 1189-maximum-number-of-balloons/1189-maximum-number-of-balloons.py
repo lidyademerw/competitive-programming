e0e7ecfec2b3c10c6197816244daae172302254a
lidya demerw <demerwlidya@gmail.com>
2026-06-21 22:08:32 -0700

    Time: 0 ms (100%), Space: 19.4 MB (9.97%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,8 +1,10 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         cou=Counter(text)
-        balloon=Counter("balloon")
-        x=len(text)
-        for i in balloon:
-            x=min(x,cou[i]//balloon[i])
-        return x
+        return min(
+            cou['b'],
+            cou['a'],
+            cou['l'] // 2,
+            cou['o'] // 2,
+            cou['n']
+        )
