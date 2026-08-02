177465b2c96861ffb0956cbe5e3b53cec0a42f94
lidya demerw <demerwlidya@gmail.com>
2026-08-01 23:32:08 -0700

    Time: 0 ms (100%), Space: 19.2 MB (93.91%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.py
@@ -0,0 +1,24 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        i=0
+        j=len( piles)-1
+        alice=0
+        bob=0
+        Alice=True
+        while i<=j:
+            if piles[i]>piles[j]:
+                value=piles[i]
+                i+=1
+            else:
+                value=piles[j]
+                j-=1
+            if Alice:
+                alice+=value
+            else:
+                bob+=1
+        return alice >=bob
+
+
+
+
+      
