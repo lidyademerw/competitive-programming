dfd95689bbef546592e9e9877b46f62b9ce9fbf9
lidya demerw <demerwlidya@gmail.com>
2026-08-03 23:00:36 -0700

    Time: 1 ms (50.66%), Space: 19.5 MB (20.78%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -7,12 +7,11 @@ class Solution:
                
             else:
                 continue
-        result=[0]*len(nums)
         index=0
         for i in range(len(nums)):
             if nums[i]!=0:
-                result[index]=nums[i]
+                nums[index],nums[i]=nums[i],nums[index]
                 index+=1
                 
-        return result
+        return nums
         
